6a1c329a6a537fce249dc22b33679995bd5026ad
lmr3796 <lmr3796joseph@gmail.com>
2013-12-10 10:17:02 -0500

    Able to execute find factor remotely

--- framework.py
+++ framework.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
+import socket
+import xmlrpclib
+
 import config
+
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 class Example(object):
@@ -9,16 +14,19 @@ class Example(object):
 
     def long_add(self, a, b):
         t = 0
-        for i in range(100000000):
+        for i in xrange(100000000):
             t += 1
         return self.add(a,b)
 
 def build_rpc_server_from_component(comp):
+    socket.setdefaulttimeout(30)
     server = SimpleXMLRPCServer(('', config.port[comp.__class__.__name__]))
     server.register_instance(comp)
     return server 
 
 if __name__ == '__main__':
+    t = TimeoutTransport()
+    t.set_timeout(100)
     server = SimpleXMLRPCServer(('', 3796))
     server.register_instance(Example())
     server.serve_forever()
