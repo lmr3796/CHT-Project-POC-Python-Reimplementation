27d749e17a5795d76308227173197c072fea3a2a
lmr3796 <lmr3796joseph@gmail.com>
2014-01-08 14:42:40 -0500

    Fix workload-based decision maker bug and refine

--- decision_maker_by_workload.py
+++ decision_maker_by_workload.py
@@ -8,32 +8,24 @@ class DecisionMakerByWorkload(object):
     def schedule_jobs(self, job_set, worker_available_status):
         try:
             print >> sys.stderr, 'Workload-based scheduling'
-            workload = [sum(j['per_server_time'].values())/float(len(j['per_server_time'])) for j in job_set]
-            total_workload = sum(workload)
-            print >> sys.stderr, 'Workload:',  workload
-            print >> sys.stderr, 'Total workload:',  total_workload
-            for idx in range(len(workload)):
-                workload[idx] = len(worker_available_status) * workload[idx] / total_workload
-            sorted_job_set = sorted([(idx, j) for idx, j in enumerate(job_set)],
-                    key=lambda x: workload[x[0]], reverse=True)
-            worker_scheduled = {w: False for w, available in worker_available_status.iteritems() if available}
-            schedule_result = [None] * len(job_set)
+            remaining_worker = [w for w, available in worker_available_status.iteritems() if available]
+            workload = [len(j['task']) * sum(j['per_server_time'].values()) / float(len(j['per_server_time'])) for j in job_set]
+            workload = map(lambda load: len(remaining_worker) * load / sum(workload), workload)  # Normalized to total of #remaining_worker
+            print >> sys.stderr, 'Workload:', workload
+            sorted_job_set = sorted([(idx, j) for idx, j in enumerate(job_set)], key=lambda x: workload[x[0]])
+            schedule_result = [[] for i in  range(len(job_set))]
+
             # First, assign each job with one worker
             for i, job in sorted_job_set:
-                print 'job[%d]: %s with %d tasks, workload %.2f' % (i,
-                        job['jobname'], len(job['task']), workload[i])
-                best_worker = None
-                for w in [w for w in worker_scheduled if not worker_scheduled[w]]:
-                    if best_worker == None:
-                        best_worker = w
-                    elif job['per_server_time'][w] < job['per_server_time'][best_worker]:
-                        best_worker = w
-                assert best_worker != None
-                schedule_result[i] = [best_worker]
-                workload[i] = workload[i] - 1.0
-                worker_scheduled[best_worker] = True
+                print >> sys.stderr, 'Job[%d]: %s with %d tasks, workload %.2f' % (i, job['jobname'], len(job['task']), workload[i])
+                best_worker = min(remaining_worker, key=lambda w:job['per_server_time'][w])
+                schedule_result[i].append(best_worker)
+                remaining_worker.remove(best_worker)
+                workload[i] = workload[i] - 1.0 # TODO: WHAT????????
+
+
             # Second, assign the rest of workers by their workload
-            while len([w for w in worker_scheduled if not worker_scheduled[w]]) > 0:
+            while remaining_worker:
                 heaviest_job_id = None
                 for idx in range(len(workload)):
                     if len(job_set[idx]['task']) == len(schedule_result[idx]):
@@ -46,16 +38,10 @@ class DecisionMakerByWorkload(object):
                 if heaviest_job_id == None:
                     break
                 job = job_set[heaviest_job_id]
-                best_worker = None
-                for w in [w for w in worker_scheduled if not worker_scheduled[w]]:
-                    if best_worker == None:
-                        best_worker = w
-                    elif job['per_server_time'][w] < job['per_server_time'][best_worker]:
-                        best_worker = w
-                assert best_worker != None
+                best_worker = min(remaining_worker, key=lambda w:job['per_server_time'][w])
                 schedule_result[heaviest_job_id].append(best_worker)
-                workload[heaviest_job_id] = workload[heaviest_job_id] - 1.0
-                worker_scheduled[best_worker] = True
+                remaining_worker.remove(best_worker)
+                workload[heaviest_job_id] = workload[heaviest_job_id] - 1.0 # TODO: WHAT????????
             return schedule_result
 
         except Exception as e:
