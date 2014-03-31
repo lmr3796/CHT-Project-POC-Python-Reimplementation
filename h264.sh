f92215e3933ddabc4fd997d2f29fb3f44d89fcab
lmr3796 <lmr3796joseph@gmail.com>
2014-03-31 14:49:05 +0800

    User can specify SPEC_ROOT from shell

--- h264.sh
+++ h264.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 
-SPEC_ROOT=/home/lmr3796/CHT/SPEC_2006/build
+SPEC_ROOT=${SPEC_ROOT:=/home/lmr3796/SPEC_BUILD}
 H264_PATH=$SPEC_ROOT/log/benchspec/CPU2006/464.h264ref/run/run_base_train_amd64-m64-gcc43-nn.0000
 
 cd $SPEC_ROOT
--- word_count.sh
+++ word_count.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
+SPEC_ROOT=${SPEC_ROOT:=/home/lmr3796/SPEC_BUILD}
 for (( i=0 ; $i<40 ; i=i+1 ))
 do
-    wc -w ~/CHT/SPEC_2006/build/benchspec/CPU2006/401.bzip2/data/all/input/input.combined > /dev/null
+    wc -w $SPEC_ROOT/benchspec/CPU2006/401.bzip2/data/all/input/input.combined #> /dev/null
 done
